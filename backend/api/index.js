89bb0985b18bd68b91a3f0125a47662f33e5a3e8
harveerdeol <harveerdeol53@gmail.com>
2026-01-29 10:43:48 -0700

    CHANGED: removed cors logic from index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,19 +1,3 @@
 const app = require("../src/server.js");
 
-module.exports = async (req, res) => {
-  const origin = req.headers.origin;
-  
-  // Allow all Vercel deployments
-  if (origin && origin.match(/^https:\/\/job-app-frontend.*\.vercel\.app$/)) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  }
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  return app(req, res);
-};
+module.exports = app;
